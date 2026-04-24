88e59a501b124a2c2ad2c473b0dd3c701c4631c3
vrraj <61334130+vrraj@users.noreply.github.com>
2026-04-23 17:31:49 -0700

    Checkpoint: Resolve Dependencies

--- bm25s_retriever/__init__.py
+++ bm25s_retriever/__init__.py
@@ -3,8 +3,17 @@ BM25S Retriever - A standalone document retrieval service.
 """
 
 from .core.retriever import BM25SRetriever, retrieve_documents
-from .api.client import BM25SClient
-from .api.models import Document, RetrieveRequest, RetrieveResponse
+
+try:
+    from .api.client import BM25SClient
+    from .api.models import Document, RetrieveRequest, RetrieveResponse
+    _HAS_API = True
+except ImportError:
+    _HAS_API = False
+    BM25SClient = None
+    Document = None
+    RetrieveRequest = None
+    RetrieveResponse = None
 
 __version__ = "1.0.0"
 __all__ = [
