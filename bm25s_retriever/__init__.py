51d2fb06523c86c70b812dd2e3974b92035a9d14
vrraj <61334130+vrraj@users.noreply.github.com>
2026-04-29 13:26:23 -0700

    Fix Document import to use core dataclass instead of pydantic model, bump version to 1.0.1

--- bm25s_retriever/__init__.py
+++ bm25s_retriever/__init__.py
@@ -2,20 +2,19 @@
 BM25S Retriever - A standalone document retrieval service.
 """
 
-from .core.retriever import BM25SRetriever, retrieve_documents
+from .core.retriever import BM25SRetriever, retrieve_documents, Document
 
 try:
     from .api.client import BM25SClient
-    from .api.models import Document, RetrieveRequest, RetrieveResponse
+    from .api.models import RetrieveRequest, RetrieveResponse
     _HAS_API = True
 except ImportError:
     _HAS_API = False
     BM25SClient = None
-    Document = None
     RetrieveRequest = None
     RetrieveResponse = None
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = [
     "BM25SRetriever",
     "retrieve_documents", 
